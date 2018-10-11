0d741d8dc746ab5f119c66a24821f74b4f4ade12
David Forer <david.j.forer@gmail.com>
2018-10-11 21:19:48 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,11 +1,10 @@
 class TicTacToe
-  
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
   
   WIN_COMBINATIONS = [
-  [0,1,2], 
+  [0,1,2], #win_combination
   [3,4,5],
   [6,7,8],
   [0,3,6],
@@ -22,8 +21,7 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-  
-  def input_to_index(userInput)
+   def input_to_index(userInput)
     index = userInput.to_i - 1
   end
    def move(index, token = "X")
@@ -43,8 +41,7 @@ class TicTacToe
       false
     end
   end
-  
-  def turn
+   def turn
     puts "Please enter 1-9:"
     userInput = gets.strip
     index = input_to_index(userInput)
@@ -55,8 +52,7 @@ class TicTacToe
       turn
     end
   end
-  
-  def turn_count
+   def turn_count
     counter = 0
     @board.each { |space|
       if space == "X" || space == "O"
@@ -65,64 +61,69 @@ class TicTacToe
     }
     counter
   end
-  
-  def current_player
+   def current_player
     if turn_count%2 == 0
       "X"
     else
       "O"
     end
-  
-  def won?
-    WIN_COMBINATIONS.each do |combo|
-     if @board[combo[0]] != " " && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
-      return combo
-     end
+  end
+   def won?
+    WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+ 
+      position_1 = @board[win_index_1] # load the value of the board at win_index_1
+      position_2 = @board[win_index_2] # load the value of the board at win_index_2
+      position_3 = @board[win_index_3] # load the value of the board at win_index_3
+    
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return win_combination # return the win_combination indexes that won.
+      end
     end
-    false
+    return FALSE
   end
    def full?
-    if @board.include?(" ")
-      false
-    else
-      true
-    end
+    @board.all? { |space| space == "X" || space == "O"}
   end
    def draw?
-    if full? == true && won? == false
-      true
+    if won? == FALSE && full? == TRUE
+      return TRUE
     else
-      false
+      return FALSE
     end
   end
    def over?
-    if draw? == true
-      return true
-    elsif full? == true && won? != false
-      return true
-    elsif full? == false && won? != false
-      return true
-    else
-      false
+    if won? != FALSE || full? == TRUE || draw? == TRUE
+      return TRUE
     end
   end
    def winner
-    if winner = won?
-      @board[winner[0]]
+    if won? != FALSE
+      index = won?
+      if @board[index[0]] == "X" || @board[index[0]] == "O"
+        return @board[index[0]]
+      end
     end
   end
    def play
-    until over? == true do
+    until over? == TRUE
       turn
-    end
       if winner == "X"
         puts "Congratulations X!"
       elsif winner == "O"
         puts "Congratulations O!"
-      elsif draw? == true
+      elsif draw? == TRUE
         puts "Cat's Game!"
+      end
+    end
+    if winner == "X"
+      puts "Congratulations X!"
+    elsif winner == "O"
+      puts "Congratulations O!"
+    elsif draw? == TRUE
+      puts "Cat's Game!"
     end
   end
-
-end
 end
